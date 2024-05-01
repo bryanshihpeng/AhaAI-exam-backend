69d16cd2ec3ce7e3c2108b74ecb6f2302ab5098c
Bryab Shih <bryan@tcbinter.com>
2024-05-01 21:35:26 +0800

    Enhance password security using hash functions.

--- src/domain/user/user.entity.ts
+++ src/domain/user/user.entity.ts
@@ -1,5 +1,6 @@
 import { BaseEntity, Entity, PrimaryKey, Property } from '@mikro-orm/core';
 import { BadRequestException } from '@nestjs/common';
+import bcrypt from 'bcrypt';
 import { v4 } from 'uuid';
 
 @Entity()
@@ -8,22 +9,30 @@ export class User extends BaseEntity {
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   @PrimaryKey()
   id = v4();
+
   @Property({ type: 'datetime', defaultRaw: 'NOW()' })
   signUpAt = new Date();
+
   @Property({ type: 'number', default: 0 })
   loginCount = 0;
+
   @Property({ type: 'datetime', defaultRaw: 'NOW()' })
   lastSessionAt?: Date;
+
   @Property({ unique: true, nullable: true })
   email: string;
+
   @Property()
   name = '';
+
   @Property({ nullable: true })
   firebaseUid?: string;
+
   @Property({ nullable: true })
   emailVerified?: boolean;
+
   @Property({ nullable: true })
-  password?: string;
+  private password?: string;
 
   static createWithEmailAndPassword(email: string, password: string): User {
     if (!email || !password) throw new Error('Email and password are required');
@@ -35,29 +44,29 @@ export class User extends BaseEntity {
   }
 
   resetPassword(password: string, newPassword: string): void {
-    if (!this.password) {
-      throw new BadRequestException('Account does not have a password');
-    }
-    if (this.password !== password) {
-      throw new BadRequestException('Invalid password');
-    }
+    this.validatePassword(newPassword);
+    this.verifyPassword(password);
     this.setPassword(newPassword);
   }
 
   setPassword(password: string): void {
-    if (!this.validatePassword(password)) {
-      throw new BadRequestException(
-        'Password does not meet complexity requirements',
-      );
+    this.validatePassword(password);
+    this.password = this.hashPassword(password);
+  }
+
+  hashPassword(password: string): string {
+    const saltRounds = 10;
+    return bcrypt.hashSync(password, saltRounds);
+  }
+
+  private verifyPassword(password: string): boolean {
+    if (!this.password) {
+      throw new BadRequestException('Account does not have a password');
     }
-    this.password = password;
+    return this.password == this.hashPassword(password);
   }
 
-  verifyPassword(password: string): boolean {
-    return this.password === password;
-  }
-
-  validatePassword(password: string): boolean {
+  private validatePassword(password: string): boolean {
     return User.passWordRules.test(password);
   }
 }
